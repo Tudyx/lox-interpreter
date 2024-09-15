e03297ed97a528b1942d6f3bba36f10eb3e32fe8
Tudyx <56633664+Tudyx@users.noreply.github.com>
2024-09-15 15:08:07 +0200

    first stage

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     match command.as_str() {
         "tokenize" => {
             // You can use print statements as follows for debugging, they'll be visible when running tests.
-            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
+            eprintln!("Logs from your program will appear here!");
 
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
