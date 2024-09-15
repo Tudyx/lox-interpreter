16d932358b07fb3b3e6c21674d27714b43114ff2
Tudyx <56633664+Tudyx@users.noreply.github.com>
2024-09-15 15:44:28 +0200

    first stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::ops::Index;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -33,7 +34,7 @@ fn main() {
 
 fn tokenize(file_content: &str) {
     let mut lexical_error = false;
-    for c in file_content.chars() {
+    for (i, c) in file_content.chars().enumerate() {
         match c {
             '(' => println!("LEFT_PAREN ( null"),
             ')' => println!("RIGHT_PAREN ) null"),
@@ -45,7 +46,18 @@ fn tokenize(file_content: &str) {
             '+' => println!("PLUS + null"),
             ';' => println!("SEMICOLON ; null"),
             '*' => println!("STAR * null"),
-            // '$' | '#' =>
+            '=' => {
+                // We assume that the text contains only ASCII charactere
+                if file_content
+                    .as_bytes()
+                    .get(i + 1)
+                    .is_some_and(|octet| *octet == b'=')
+                {
+                    println!("EQUAL_EQUAL == null");
+                } else {
+                    println!("EQUAL = null")
+                }
+            }
             c => {
                 eprintln!("[line 1] Error: Unexpected character: {c}");
                 lexical_error = true;
