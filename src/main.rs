ad7083376ea5c179285674521f1c6ae6ec983252
Tudyx <56633664+Tudyx@users.noreply.github.com>
2024-09-15 17:02:50 +0200

    first stage

--- src/main.rs
+++ src/main.rs
@@ -79,6 +79,7 @@ fn tokenize(file_content: &str) {
                 // We ignore the rest of the line
                 if chars.next_if_eq(&'/').is_some() {
                     for _ in chars.by_ref().take_while(|c| c != &'\n') {}
+                    line_count += 1;
                 } else {
                     println!("SLASH / null")
                 }
